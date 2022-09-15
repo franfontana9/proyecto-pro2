8fb6687c766d7ce326334d005bab234617412580
santiagoscilingo <santiago.scilingo>
2022-09-15 16:54:05 -0300

    ultimos cambios

--- proyecto/controllers/userController.js
+++ proyecto/controllers/userController.js
@@ -8,14 +8,14 @@ const userController = {
   },
 
   editarPerfil: function (req, res) {
-    res.render('editarPerfil',{info: data.usuarios, personal: req.params.id });
+    res.render('editarPerfil',{info: data.usuarios, personal: req.params.id});
   },
   login: function (req, res) {
     res.render('login');
   },
 
   miPerfil: function (req, res) {
-    res.render('miPerfil', {info: data.usuarios, posteos: data.posteos});
+    res.render('miPerfil', {info: data.usuarios[0], posteos: data.posteos});
   },
   registracion: function (req, res) {
     res.render('registracion');
